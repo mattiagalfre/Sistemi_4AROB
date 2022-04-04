8dcfa9274e148a2265ce3a96b301995187ec8115
Mattia Galfrè <90908043+mattiagalfre@users.noreply.github.com>
2022-04-04 08:44:53 +0200

    Delete dizionari.py

--- Python/dizionari.py
+++
@@ -1,9 +0,0 @@
-#ogni elemento di un dizionario è una coppia
-#ogni coppia è costituitada una chiave e un valore
-
-dizionario = {"to get":"prendere",
-                "hello":"ciao","print":"stampa"}
-
-#nei dizionari non si usano indici
-#si accede per chiave
-print(dizionario["hello"])
