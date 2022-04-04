3e3fd586f63d6eb0358dedf5e11f6ecd7b3e5d42
Mattia Galfrè <90908043+mattiagalfre@users.noreply.github.com>
2022-04-04 08:45:31 +0200

    Delete es003.py

--- Python/es003.py
+++
@@ -1,7 +0,0 @@
-nome = input("Dimmi il tuo nome: ")
-anni = int(input("Dimmi la tua età: "))
-
-print(f"Tu ti chiami {nome} e hai {anni} anni.")
-print(f"Il tuo nome comincia per {nome[0]}")
-print(f"Il tuo nome finisce per {nome[-1]}")
-print(f"La penultima lettera del tuo nome è {nome[-2]}")
