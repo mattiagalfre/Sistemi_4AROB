9fc283bb6e22cac90d14fd3eae52e5c8c76ac969
Mattia Galfrè <90908043+mattiagalfre@users.noreply.github.com>
2022-04-04 08:44:46 +0200

    Delete cicloFor.py

--- Python/cicloFor.py
+++
@@ -1,5 +0,0 @@
-numeri_primi = [2,3,5,7,11,13]
-
-for numero_primo in numeri_primi:
-    print(numero_primo, end="-")        #modofico il carattere finale, di norma è l'invio a capo
-print("FINE")   
